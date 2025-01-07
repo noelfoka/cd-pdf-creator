f9c7fd08e17614d067080d352b772a4f2275fff0
noelfoka <nomgnenoel@gmail.com>
2025-01-07 11:17:33 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+// import Image from "next/image";
+
 export default function Home() {
   return (
     <div>
@@ -7,6 +9,8 @@ export default function Home() {
           <div className="hero-content text-center">
             <div className="max-w-md">
               <h1 className="text-3xl font-bold">Désolé, le CV builder est uniquement disponible sur desktop.</h1>
+             {/* <Image src="/emoji.gif" alt="Gift" width={500} height={500} className="mx-auto my-6" />*/}
+              <div className="tenor-gif-embed" data-postid="1437772575988315493" data-share-method="host" data-aspect-ratio="1" data-width="100%"><a href="https://tenor.com/view/sorry-im-sorry-im-so-sorry-oops-apologies-gif-1437772575988315493">Sorry Im Sorry Sticker</a>from <a href="https://tenor.com/search/sorry-stickers">Sorry Stickers</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
               <p className="py-6">
                 Pour creer et personnaliser votre CV, veuillez utiliser un ordinateur. Nous vous remercions de votre compréhension.
               </p>
