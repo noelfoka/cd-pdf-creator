1fcd461284c0180b3367896fae956d4799832051
noelfoka <nomgnenoel@gmail.com>
2025-01-07 16:29:37 +0100

    opdateee

--- app/components/CVPreview.tsx
+++ app/components/CVPreview.tsx
@@ -14,19 +14,17 @@ const CVPreview: React.FC<Props> = ({ personalDetails, file }) => {
   return (
     <div className={`flex p-16 w-[950px] h-[1200px] shadow-lg`}>
       <div className="flex flex-col w-1/3">
-
         <div className="h-80 rounded-full border-8 overflow-hidden border-primary">
           {file && (
             <Image
               src={URL.createObjectURL(file)}
               alt="Profile"
-
               width={300}
               height={300}
               className="w-full h-full rounded-lg object-cover"
               onLoadingComplete={() => {
-                if(typeof file !== 'string') {
-                  URL.revokeObjectURL(URL.createObjectURL(file))
+                if (typeof file !== "string") {
+                  URL.revokeObjectURL(URL.createObjectURL(file));
                 }
               }}
             />
@@ -35,16 +33,12 @@ const CVPreview: React.FC<Props> = ({ personalDetails, file }) => {
 
         <div className="mt-4 flex flex-col w-full">
           <div>
-            <h1 className="uppercase font-bold my-2">
-              contact
-            </h1>
+            <h1 className="uppercase font-bold my-2">contact</h1>
 
             <ul className="space-y-2">
               <li className="flex">
                 <div className="break-all text-sm relative">
-                  <div className="ml-8">
-                    {personalDetails.phone}
-                  </div>
+                  <div className="ml-8">{personalDetails.phone}</div>
                   {personalDetails.phone && (
                     <div className="absolute left-0 top-0">
                       <Phone className="w-5 text-primary" />
@@ -55,9 +49,7 @@ const CVPreview: React.FC<Props> = ({ personalDetails, file }) => {
 
               <li className="flex">
                 <div className="break-all text-sm relative">
-                  <div className="ml-8">
-                    {personalDetails.email}
-                  </div>
+                  <div className="ml-8">{personalDetails.email}</div>
                   {personalDetails.email && (
                     <div className="absolute left-0 top-0">
                       <Mail className="w-5 text-primary" />
@@ -68,9 +60,7 @@ const CVPreview: React.FC<Props> = ({ personalDetails, file }) => {
 
               <li className="flex">
                 <div className="break-all text-sm relative">
-                  <div className="ml-8">
-                    {personalDetails.address}
-                  </div>
+                  <div className="ml-8">{personalDetails.address}</div>
                   {personalDetails.address && (
                     <div className="absolute left-0 top-0">
                       <MapPinCheckInside className="w-5 text-primary" />
@@ -78,14 +68,18 @@ const CVPreview: React.FC<Props> = ({ personalDetails, file }) => {
                   )}
                 </div>
               </li>
-
             </ul>
           </div>
         </div>
-
       </div>
-      
-      <div></div>
+
+      <div className="w-2/3 ml-8">
+        <div className="w-full flex flex-col space-y-4">
+          <h1>{personalDetails.fullName}</h1>
+          <h2>{personalDetails.postSeeking}</h2>
+          <p>{personalDetails.description}</p>
+        </div>
+      </div>
     </div>
   );
 };
