e4019695bd479f0eef20458c3d53ee6b8dfc76fc
noelfoka <nomgnenoel@gmail.com>
2025-01-07 11:00:12 +0100

    page d'avertissement

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
 export default function Home() {
   return (
     <div>
-      <button className="btn btn-primary">Primary</button>
+      <div className="lg:hidden">
+
+        <div className="hero bg-base-200 min-h-screen">
+          <div className="hero-content text-center">
+            <div className="max-w-md">
+              <h1 className="text-3xl font-bold">Désolé, le CV builder est uniquement disponible sur desktop.</h1>
+              <p className="py-6">
+                Pour creer et personnaliser votre CV, veuillez utiliser un ordinateur. Nous vous remercions de votre compréhension.
+              </p>
+            </div>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
