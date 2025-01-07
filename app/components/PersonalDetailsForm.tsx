9325af36e78ef4e9f4ee5f349d3edc8ccd9c5dfd
noelfoka <nomgnenoel@gmail.com>
2025-01-07 14:50:15 +0100

    1 2 3 4 input

--- app/components/PersonalDetailsForm.tsx
+++ app/components/PersonalDetailsForm.tsx
@@ -12,6 +12,12 @@ const PersonalDetailsForm: React.FC<Props> = ({
   setPersonalDetails,
   setFile,
 }) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    field: keyof PersonalDetails
+  ) => {
+    setPersonalDetails({ ...personalDetails, [field]: e.target.value });
+  };
   return (
     <div className="flex flex-col gap-4">
       <input
@@ -19,7 +25,40 @@ const PersonalDetailsForm: React.FC<Props> = ({
         placeholder="Nom complet"
         value={personalDetails.fullName}
         className="input input-bordered w-full"
-        onChange={(e) => setPersonalDetails({ ...personalDetails, fullName: e.target.value })}
+        onChange={(e) => handleChange(e, "fullName")}
+      />
+
+      <div className="flex">
+        <input
+          type="email"
+          placeholder="Email"
+          value={personalDetails.email}
+          className="input input-bordered w-full"
+          onChange={(e) => handleChange(e, "email")}
+        />
+
+        <input
+          type="text"
+          placeholder="Numéro de téléphone"
+          value={personalDetails.phone}
+          className="input input-bordered w-full ml-2"
+          onChange={(e) => handleChange(e, "phone")}
+        />
+      </div>
+
+      <input
+        type="text"
+        placeholder="Adresse"
+        value={personalDetails.address}
+        className="input input-bordered w-full"
+        onChange={(e) => handleChange(e, "address")}
+      />
+
+      <input
+        type="file"
+        accept="image/*"
+        className="file-input file-input-bordered w-full"
+        //onChange={(e) => handleChange(e, "photoUrl")}
       />
     </div>
   );
