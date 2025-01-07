e3f4fa2cc7ac3be497c871443073da7dd9ee0d52
noelfoka <nomgnenoel@gmail.com>
2025-01-07 15:53:33 +0100

    add image to preview

--- app/components/CVPreview.tsx
+++ app/components/CVPreview.tsx
@@ -1,19 +1,39 @@
-import { PersonalDetails } from '@/type'
-import React from 'react'
+"use client";
+
+import { PersonalDetails } from "@/type";
+import React from "react";
+import Image from "next/image";
 
 type Props = {
   personalDetails: PersonalDetails;
   file: File | null;
-}
+};
 
-const CVPreview: React.FC<Props> = ({personalDetails, file}) => {
+const CVPreview: React.FC<Props> = ({ personalDetails, file }) => {
   return (
     <div className={`flex p-16 w-[950px] h-[1200px] shadow-lg`}>
+      <div className="flex flex-col w-1/3">
+        <div className="h-80 rounded-full border-8 overflow-hidden border-primary">
+          {file && (
+            <Image
+              src={URL.createObjectURL(file)}
+              alt="Profile"
 
-      <div className='flex flex-col w-1/3'></div>
-
+              width={300}
+              height={300}
+              className="w-full h-full rounded-lg object-cover"
+              onLoadingComplete={() => {
+                if(typeof file !== 'string') {
+                  URL.revokeObjectURL(URL.createObjectURL(file))
+                }
+              }}
+            />
+          )}
+        </div>
+        <div></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CVPreview
+export default CVPreview;
