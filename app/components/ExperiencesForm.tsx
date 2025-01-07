986e4ba5b2afb975e4d7991472b88091f828fa07
noelfoka <nomgnenoel@gmail.com>
2025-01-07 19:09:49 +0100

    add many input

--- app/components/ExperiencesForm.tsx
+++ app/components/ExperiencesForm.tsx
@@ -1,5 +1,5 @@
-import { Experience } from '@/type';
-import React from 'react'
+import { Experience } from "@/type";
+import React, { useState } from "react";
 
 type Props = {
   experience: Experience[];
@@ -7,9 +7,63 @@ type Props = {
 };
 
 const ExperiencesForm: React.FC<Props> = ({ experience, setExperience }) => {
-  return (
-    <div>ExperiencesForm</div>
-  )
-}
+  const [newExperience, setNewExperience] = useState<Experience>({
+    jobTitle: "",
+    companyName: "",
+    startDate: "",
+    endDate: "",
+    description: "",
+  });
 
-export default ExperiencesForm
+  const handleNewExperience = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    field: keyof Experience
+  ) => {
+    setNewExperience({ ...newExperience, [field]: e.target.value });
+  };
+
+  return (
+    <div className="flex flex-col gap-4">
+
+      <div className="flex justify-between">
+        <input
+          type="text"
+          value={newExperience.jobTitle}
+          onChange={(e) => handleNewExperience(e, "jobTitle")}
+          placeholder="Titre du job"
+          className="input input-bordered w-full"
+        />
+
+        <input
+          type="text"
+          value={newExperience.companyName}
+          onChange={(e) => handleNewExperience(e, "companyName")}
+          placeholder="Nom de l'entreprise"
+          className="input input-bordered w-full ml-2"
+        />
+      </div>
+
+      <div className="flex justify-between">
+        <input
+          type="text"
+          value={newExperience.jobTitle}
+          onFocus={(e) => e.target.type = "Date"}
+          onChange={(e) => handleNewExperience(e, "jobTitle")}
+          placeholder="Titre du job"
+          className="input input-bordered w-full"
+        />
+
+        <input
+          type="text"
+          value={newExperience.companyName}
+          onChange={(e) => handleNewExperience(e, "companyName")}
+          placeholder="Nom de l'entreprise"
+          className="input input-bordered w-full ml-2"
+        />
+      </div>
+
+    </div>
+  );
+};
+
+export default ExperiencesForm;
