beec0fdf4c1fadca6fb64d7410a3db7fc577552f
noelfoka <nomgnenoel@gmail.com>
2025-01-08 12:52:51 +0100

    create props type to language

--- app/components/LanguageForm.tsx
+++ app/components/LanguageForm.tsx
@@ -1,6 +1,12 @@
+import { Language } from '@/type';
 import React from 'react'
 
-const LanguageForm = () => {
+type Props = {
+  language: Language[];
+  setLanguage: (language: Language[]) => void;
+}
+
+const LanguageForm: React.FC<Props> = ({ language, setLanguage }) => {
   return (
     <div>LanguageForm</div>
   )
