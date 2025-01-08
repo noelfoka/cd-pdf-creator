5fdccaeb8d6bda21624c1ead499b203af07a6afc
noelfoka <nomgnenoel@gmail.com>
2025-01-08 13:11:51 +0100

    ajout des champs

--- app/components/LanguageForm.tsx
+++ app/components/LanguageForm.tsx
@@ -14,7 +14,7 @@ const LanguageForm: React.FC<Props> = ({ language, setLanguage }) => {
   });
 
   const handleNewLanguage = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
     field: keyof Language
   ) => {
     setNewLanguage({ ...newLanguage, [field]: e.target.value });
@@ -29,7 +29,21 @@ const LanguageForm: React.FC<Props> = ({ language, setLanguage }) => {
   };
 
   return (
-    <div>LanguageForm</div>
+    <div className='space-y-4'>
+      <input
+            type="text"
+            value={newLanguage.language}
+            onChange={(e) => handleNewLanguage(e, "language")}
+            placeholder="Langue"
+            className="input input-bordered w-full"
+          />
+
+          <select
+            value={newLanguage.proficiency}
+            onChange={(e) => handleNewLanguage(e, "proficiency")}
+            className="select select-bordered w-full"
+          ></select>
+    </div>
   )
 }
 
