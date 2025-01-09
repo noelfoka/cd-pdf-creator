1a372adc9a2121ab03ba3fef443b94fb6dfe89a4
noelfoka <nomgnenoel@gmail.com>
2025-01-09 03:20:24 +0100

    add props type

--- app/components/SkillsForm.tsx
+++ app/components/SkillsForm.tsx
@@ -1,6 +1,12 @@
+import { Skill } from '@/type';
 import React from 'react'
 
-const SkillsForm = () => {
+type Props = {
+  skills: Skill[];
+  setSkills: (skills: Skill[]) => void;
+}
+
+const SkillsForm: React.FC<Props> = ({ skills, setSkills }) => {
   return (
     <div>SkillsForm</div>
   )
